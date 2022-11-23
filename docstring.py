2b345bbae20296c3c9150d6718593cd70f80842c
Магомедов Марат <magomedov_it@mail.ru>
2022-11-23 22:01:25 +0300

    Docstring

---
+++ docstring.py
@@ -0,0 +1,24 @@
+from math import sqrt
+
+message = (
+    'Добро пожаловать в самую лучшую программу для вычисления '
+    'квадратного корня из заданного числа.')
+
+
+def calculatesquareroot(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Что-то да и делает."""
+    if your_number <= 0:
+        return
+    result = calculatesquareroot(your_number)
+    print(
+        f'Мы вычислили квадратный корень из введённого вами числа. '
+        f'Это будет: {result}')
+
+
+print(message)
+calc(25.5)
